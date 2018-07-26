539555cf56686960af5ab6da7a63d929d1735351
vinitraj10 <vinitraj1012@gmail.com>
2018-07-26 21:08:11 +0530

    Changes for redux in index file

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 // redux imports
-import {createStore} from 'redux';
+import {createStore,applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 
 import reducers from './reducers';
 
+import ReduxThunk from "redux-thunk";
+
 import MainApp from './components';
 
+const store = applyMiddleware(ReduxThunk)(createStore);
 
 render(
-	<Provider store={createStore(reducers)}>
+	<Provider store={store(reducers)}>
 		<MainApp/>
 	</Provider>
 	,document.getElementById('root'));
