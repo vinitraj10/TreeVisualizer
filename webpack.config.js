159a340729e009d28fd36fca0ea832132bb9ff2c
vinitraj10 <vinitraj1012@gmail.com>
2018-07-26 21:06:57 +0530

    webpack configuration changed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 var APP_DIR = path.resolve(__dirname,'src');
-var BUILD_DIR = path.resolve(__dirname,'dist');
+var BUILD_DIR = path.resolve(__dirname,'dist/js');
 
 var config = {
 	entry : APP_DIR + '/index.js',
