fc7e1c25a888639d209b83ab770b18ff56f47745
vinitraj10 <vinitraj1012@gmail.com>
2018-07-27 18:38:06 +0530

    Diagram Component Finished

--- src/containers/livetree.js
+++ src/containers/livetree.js
@@ -1,15 +1,42 @@
 import React,{Component} from 'react';
 import Tree from 'react-tree-graph';
 import {connect} from 'react-redux';
-import {traverseTree} from '../actions';
+import {updateDiagram} from '../actions';
 
 
 class Livetree extends Component {
+	constructor(){
+		super();
+		this.traverseSubTree = this.traverseSubTree.bind(this);
+	}
 	componentWillUpdate(nextProps){
 		const {list} = nextProps.traversed;
 		const {data} = nextProps;
 		if(list.length>=1)
-			nextProps.traverseTree(list[list.length-1],data);
+			this.traverseTree(list[list.length-1],data);
+	}
+	traverseTree(node,data){
+		if(node==data.name){
+			data.circleProps = { fill: "blue" };
+			this.props.updateDiagram(data);
+		}
+		else{
+			const subTree = this.traverseSubTree(node,data);
+			this.props.updateDiagram(subTree);
+		}
+	}
+	traverseSubTree(node,data){
+		const children = data.children;
+		children.forEach((obj)=>{
+			if(obj.name==node){
+				obj.circleProps = {fill:"blue"}
+				return;
+			}
+			else if(obj.hasOwnProperty('children')){
+				this.traverseSubTree(node,obj);
+			}
+		});
+		return data;
 	}
 	render(){
 		const {data} = this.props;
@@ -39,4 +66,4 @@ function mapStateToProps(state){
 		data : state.dgmData.data
 	}
 }
-export default connect(mapStateToProps,{traverseTree})(Livetree);
+export default connect(mapStateToProps,{updateDiagram})(Livetree);
